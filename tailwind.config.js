40472e74b49011456e4f1d89240bc98cf685cef0
serpiko <serpiko@hanmail.net>
2022-06-02 19:48:57 +0900

    config update

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    // For the best performance and to avoid false positives, 최상의 성능과 오탐 방지를 위해,
+    // be as specific as possible with your content configuration. 콘첸트 구성은 가능한 한 구체적이어야 한다.
+  ],
+};
